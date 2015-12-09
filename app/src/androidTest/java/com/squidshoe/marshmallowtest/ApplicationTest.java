6e49e7b39e89191e5ae68e1d71a468d4b5a3625f
Kevin Kovach <kovach@gmail.com>
2015-12-09 01:25:04 -0500

    Added simplest app and library for Marshmallow testing. Screw you Pat! :-P

---
+++ app/src/androidTest/java/com/squidshoe/marshmallowtest/ApplicationTest.java
@@ -0,0 +1,18 @@
+package com.squidshoe.marshmallowtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.support.test.runner.AndroidJUnit4;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+
+    @Test
+    public void test() {
+
+    }
+}
